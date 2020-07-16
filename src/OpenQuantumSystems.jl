31bf1d7ef08d7f0f1e446f3f2f2d271838014784
hermanda <daniel.herman19@gmail.com>
2020-07-16 16:21:18 +0200

    Minimal working copy of QuantumOpticsBase.jl

--- src/OpenQuantumSystems.jl
+++ src/OpenQuantumSystems.jl
@@ -1,18 +1,45 @@
 module OpenQuantumSystems
-export hello, domath
 
-"""
-    hello(who::String)
+using SparseArrays, LinearAlgebra
+import LinearAlgebra: mul!, rmul!
 
-Return "Hello, `who`".
-"""
-hello(who::String) = "Hello, $who"
+export 
+    bases, Basis, GenericBasis, CompositeBasis, basis,
+        tensor, ⊗, permutesystems, @samebases,
+    states, StateVector, Bra, Ket, basisstate, norm,
+        dagger, normalize, normalize!,
+    operators, AbstractOperator, DataOperator, expect, variance,
+        identityoperator, ptrace, embed, dense, tr, sparse,
+    operators_dense, Operator, DenseOperator, DenseOpType, projector, dm,
+    operators_sparse, SparseOperator, diagonaloperator, SparseOpType,
+    superoperators, SuperOperator, DenseSuperOperator, DenseSuperOpType,
+        SparseSuperOperator, SparseSuperOpType, spre, spost, liouvillian,
+    fock, FockBasis, number, destroy, create,
+        fockstate, coherentstate, coherentstate!, displace,
+    spin, SpinBasis, sigmax, sigmay, sigmaz, sigmap, sigmam, spinup, spindown,
+    randstate, randoperator, thermalstate, coherentthermalstate, phase_average, passive_state,
+    nlevel, NLevelBasis, transition, nlevelstate,
+    metrics, tracenorm, tracenorm_h, tracenorm_nh,
+        tracedistance, tracedistance_h, tracedistance_nh,
+        entropy_vn, entropy_renyi, fidelity, ptranspose, PPT,
+        negativity, logarithmic_negativity, entanglement_entropy,
+    PauliBasis, PauliTransferMatrix, DensePauliTransferMatrix,
+        ChiMatrix, DenseChiMatrix, avg_gate_fidelity
 
-"""
-    domath(x::Number)
 
-Return `x + 5`.
-"""
-domath(x::Number) = x + 5
+include("sortedindices.jl")
+include("bases.jl")
+include("states.jl")
+include("operators.jl")
+include("operators_dense.jl")
+include("sparsematrix.jl")
+include("operators_sparse.jl")
+include("superoperators.jl")
+include("spin.jl")
+include("fock.jl")
+include("state_definitions.jl")
+include("pauli.jl")
+include("metrics.jl")
+include("nlevel.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,43 @@
-using Test, OpenQuantumSystems
+names = [
+    "test_sortedindices.jl",
 
-@test hello("Julia") == "Hello, Julia"
-@test domath(2.0) ≈ 7.0
+    "test_bases.jl",
+    "test_states.jl",
+
+    "test_operators.jl",
+    "test_operators_dense.jl",
+    "test_sparsematrix.jl",
+    "test_operators_sparse.jl",
+
+    
+    "test_fock.jl",
+    "test_spin.jl",
+    "test_nlevel.jl",
+    "test_state_definitions.jl",
+
+    "test_metrics.jl",
+
+    "test_superoperators.jl",
+
+    "test_pauli.jl"     
+]
+
+detected_tests = filter(
+    name->startswith(name, "test_") && endswith(name, ".jl"),
+    readdir("."))
+
+unused_tests = setdiff(detected_tests, names)
+if length(unused_tests) != 0
+    error("The following tests are not used:\n", join(unused_tests, "\n"))
+end
+
+unavailable_tests = setdiff(names, detected_tests)
+if length(unavailable_tests) != 0
+    error("The following tests could not be found:\n", join(unavailable_tests, "\n"))
+end
+
+for name=names
+    if startswith(name, "test_") && endswith(name, ".jl")
+        include(name)
+    end
+end
