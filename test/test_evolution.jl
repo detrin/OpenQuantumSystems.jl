974bf7cc366e6d74960c5c642bc8043b0346846d
hermanda <daniel.herman19@gmail.com>
2021-05-07 01:08:18 +0200

    Added exact evolution

--- test/test_evolution.jl
+++ test/test_evolution.jl
@@ -17,7 +17,8 @@ using Random, SparseArrays, LinearAlgebra
     mol2 = Molecule([mode1], 2, Energy)
     agg = Aggregate([mol1, mol2])
     aggInds = getIndices(agg; groundState=false)
-    basis = GenericBasis([length(aggInds)])
+    aggIndsLen = length(aggInds)
+    basis = GenericBasis([aggIndsLen])
     FCFact = getFranckCondonFactors(agg, aggInds; groundState=false)
     Ham = getAggHamiltonian(agg, aggInds, FCFact; groundState=false)
 
@@ -74,24 +75,24 @@ using Random, SparseArrays, LinearAlgebra
     ket1 = U_op1 * ket
     ket2 = U_op2 * ket
     ket3 = U_op3 * ket
-    ket_array = evolutionExact(ket, Ham, tspan)
+    ket_array = evolutionExact(ket, tspan, Ham)
     @test 1e-12 > D(ket1, ket_array[1])
     @test 1e-12 > D(ket2, ket_array[2])
     @test 1e-12 > D(ket3, ket_array[3])
 
     ket_array = Array{Array{ComplexF32,1},1}(undef, 0)
-    evolutionExact!(ket_array, ket, Ham, tspan)
+    evolutionExact!(ket_array, ket, tspan, Ham)
     @test 1e-7 > D(ket1.data, ket_array[1])
     @test 1e-7 > D(ket2.data, ket_array[2])
     @test 1e-7 > D(ket3.data, ket_array[3])
 
-    ket_array = evolutionApproximate(ket, Ham, tspan)
+    ket_array = evolutionApproximate(ket, tspan, Ham)
     @test 1e-12 > D(ket1, ket_array[1])
     @test 1e-12 > D(ket2, ket_array[2])
     @test 1e-12 > D(ket3, ket_array[3])
 
     ket_array = Array{Array{ComplexF32,1},1}(undef, 0)
-    evolutionApproximate!(ket_array, ket, Ham, tspan)
+    evolutionApproximate!(ket_array, ket, tspan, Ham)
     @test 1e-7 > D(ket1.data, ket_array[1])
     @test 1e-7 > D(ket2.data, ket_array[2])
     @test 1e-7 > D(ket3.data, ket_array[3])
@@ -100,26 +101,63 @@ using Random, SparseArrays, LinearAlgebra
     op1 = U_sop1 * op
     op2 = U_sop2 * op
     op3 = U_sop3 * op
-    op_array = evolutionExact(op, Ham, tspan)
+    op_array = evolutionExact(op, tspan, Ham)
     @test 1e-12 > D(op1, op_array[1])
     @test 1e-12 > D(op2, op_array[2])
     @test 1e-12 > D(op3, op_array[3])
 
     op_array = Array{Array{ComplexF32,2},1}(undef, 0)
-    evolutionExact!(op_array, op, Ham, tspan)
+    evolutionExact!(op_array, op, tspan, Ham)
     @test 1e-7 > D(op1.data, op_array[1])
     @test 1e-7 > D(op2.data, op_array[2])
     @test 1e-7 > D(op3.data, op_array[3])
 
-    op_array = evolutionApproximate(op, Ham, tspan)
+    op_array = evolutionApproximate(op, tspan, Ham)
     @test 1e-12 > D(op1, op_array[1])
     @test 1e-12 > D(op2, op_array[2])
     @test 1e-12 > D(op3, op_array[3])
 
     op_array = Array{Array{ComplexF32,2},1}(undef, 0)
-    evolutionApproximate!(op_array, op, Ham, tspan)
+    evolutionApproximate!(op_array, op, tspan, Ham)
     @test 1e-7 > D(op1.data, op_array[1])
     @test 1e-7 > D(op2.data, op_array[2])
     @test 1e-7 > D(op3.data, op_array[3])
+
+
+    data = Matrix(Hermitian(rand(ComplexF64, aggIndsLen, aggIndsLen)))
+    rho0 = DenseOperator(basis, basis, data)
+    normalize!(rho0)
+
+    tspan = [0.0:0.1:1.0;]
+    rho_t_ref = evolutionExact(rho0, tspan, Ham)
+
+    @time T, rho_t = evolution_exact(rho0, tspan, Ham; diagonalize=false)
+    U_op_array = evolutionOperatorArray(Ham, tspan)
+    for t_i in 1:length(tspan)
+        U_op = U_op_array[t_i]
+        rho_ref = U_op * rho0 * U_op'
+        1e-12 > D(rho_t_ref[t_i], rho_t[t_i])
+    end
+
+    @time T, rho_t = evolution_exact(rho0, tspan, Ham; diagonalize=true)
+    for t_i in 1:length(tspan)
+        U_op = U_op_array[t_i]
+        rho_ref = U_op * rho0 * U_op'
+        1e-12 > D(rho_t_ref[t_i], rho_t[t_i])
+    end
+
+    @time T, rho_t = evolution_approximate(rho0, tspan, Ham; diagonalize=false)
+    for t_i in 1:length(tspan)
+        U_op = U_op_array[t_i]
+        rho_ref = U_op * rho0 * U_op'
+        1e-12 > D(rho_t_ref[t_i], rho_t[t_i])
+    end
+
+    @time T, rho_t = evolution_approximate(rho0, tspan, Ham; diagonalize=true)
+    for t_i in 1:length(tspan)
+        U_op = U_op_array[t_i]
+        rho_ref = U_op * rho0 * U_op'
+        1e-12 > D(rho_t_ref[t_i], rho_t[t_i])
+    end
     
 end
