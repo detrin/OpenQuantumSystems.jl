1d2ae01f39869cd29934b6a621821ac1a9e028a1
hermanda <daniel.herman19@gmail.com>
2021-05-10 14:26:28 +0200

    Missing argument

--- src/timeevolution_base.jl
+++ src/timeevolution_base.jl
@@ -141,7 +141,7 @@ function integrate_delayed(
         save_start = false,
     )
 
-    prob = DelayDiffEq.DDEProblem{true}(df_, x0, h, (tspan[1], tspan[end]))
+    prob = DelayDiffEq.DDEProblem{true}(df_, x0, h, (tspan[1], tspan[end]), p)
 
     if steady_state
         affect! = function (integrator)
