9a603587536f7d04b8a9d60c09a49363779f664d
hermanda <daniel.herman19@gmail.com>
2021-05-11 00:11:13 +0200

    Added diagonalization for thermal state

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,7 +27,7 @@ if length(unavailable_tests) != 0
     error("The following tests could not be found:\n", join(unavailable_tests, "\n"))
 end
 
-# names = ["test_master.jl"]
+# names = ["test_initial_state.jl"]
 
 for name in names
     if startswith(name, "test_") && endswith(name, ".jl")
