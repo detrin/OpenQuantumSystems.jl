54c5d714087b0ba447d503fbc410c67a5cbe667e
hermanda <daniel.herman19@gmail.com>
2021-05-11 12:01:01 +0200

    Added evolutio operators with diag Ham

--- src/interaction_picture.jl
+++ src/interaction_picture.jl
@@ -16,3 +16,9 @@ function getInteractionHamIPictureA(
     U_op = evolutionOperator(Ham_S, t)
     return (U_op' * Ham_int * U_op).data
 end
+
+function getInteractionHamIPictureA(Ham_int::Array, H_lambda::Array, H_S::Array, H_Sinv::Array, t::AbstractFloat)
+    U_op = evolutionOperatorA(H_lambda, H_S, H_Sinv, t)
+    U_opd = adjoint(U_op)
+    return U_opd * Ham_int * U_op
+end
