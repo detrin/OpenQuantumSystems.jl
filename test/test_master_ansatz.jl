c08cba65d6de7465d7775ab8bfb494fdf918ec3c
hermanda <daniel.herman19@gmail.com>
2021-11-12 00:49:02 +0100

    Fixing tests

--- test/test_master_ansatz.jl
+++ test/test_master_ansatz.jl
@@ -21,20 +21,20 @@ import QuantumOpticsBase
     mol1 = Molecule([mode1], 3, [12500., 12750.])
     mol2 = Molecule([mode1], 3, [12500., 12800.])
     agg = Aggregate([mol1, mol2])
-    aggInds = getIndices(agg; groundState = false)
+    aggInds = getIndices(agg; groundState = true)
     vibindices = getVibIndices(agg, aggInds)
     aggIndsLen = length(aggInds)
     basis = GenericBasis([aggIndsLen])
-    FCFact = getFranckCondonFactors(agg, aggInds; groundState = false)
-    FCProd = getFCProd(agg, FCFact, aggInds, vibindices; groundState = false)
-    Ham = getAggHamiltonian(agg, aggInds, FCFact; groundState = false)
+    FCFact = getFranckCondonFactors(agg, aggInds; groundState = true)
+    FCProd = getFCProd(agg, FCFact, aggInds, vibindices; groundState = true)
+    Ham = getAggHamiltonian(agg, aggInds, FCFact; groundState = true)
 
     Ham_bath = getAggHamiltonianBath(agg)
     Ham_sys = getAggHamiltonianSystem(agg; groundState = false)
     b_sys = GenericBasis([size(Ham_sys, 1)])
     b_bath = GenericBasis([size(Ham_bath, 1)])
 
-    Ham_int = getAggHamiltonianInteraction(agg, aggInds, FCFact; groundState = false)
+    Ham_int = getAggHamiltonianInteraction(agg, aggInds, FCFact; groundState = true)
     Ham_S = Ham - Ham_int
 
     H_lambda, H_S = eigen(Ham_S.data)
@@ -53,22 +53,22 @@ import QuantumOpticsBase
 
     T = 300
     mu_array = [[2, 1]]
-    W01 = thermal_state(T, mu_array, Ham, aggInds; diagonalize=false, diagonal=true)
+    W01 = thermal_state(T, mu_array, Ham, vibindices, aggInds; diagonalize=false, diagonal=true)
     rho_traced1 = trace_bath(W01.data, agg, FCProd, aggInds, vibindices; groundState=false)
     mu_array = [[1, 2]]
-    W02 = thermal_state(T, mu_array, Ham, aggInds; diagonalize=false, diagonal=true)
+    W02 = thermal_state(T, mu_array, Ham, vibindices, aggInds; diagonalize=false, diagonal=true)
     rho_traced2 = trace_bath(W02.data, agg, FCProd, aggInds, vibindices; groundState=false)
 
     W0 = 1.0*W01 + 0.0*W02 
-    W0_bath = get_rho_bath(W0, agg, FCProd, aggInds, vibindices; groundState=false)
-    rho0 = trace_bath(W0, agg, FCProd, aggInds, vibindices; groundState = false)
+    W0_bath = get_rho_bath(W0, agg, FCProd, aggInds, vibindices; groundState=true)
+    rho0 = trace_bath(W0, agg, FCProd, aggInds, vibindices; groundState=false)
 
     W0 = DenseOperator(W0.basis_l, W0.basis_r, complex(W0.data))
     W0_bath = DenseOperator(W0_bath.basis_l, W0_bath.basis_r, complex(W0_bath.data))
     rho0 = DenseOperator(rho0.basis_l, rho0.basis_r, complex(rho0.data))
 
     p = (Ham_S, Ham_int, H_lambda, H_S, H_Sinv, W0, W0_bath, agg, FCProd, aggInds, vibindices, false, ComplexF64)
-    @time T, rho_t = master_ansatz(
+    Tspan, rho_t = master_ansatz(
         rho0,
         tspan,
         p;
@@ -78,7 +78,7 @@ import QuantumOpticsBase
     )
     rho_prev = deepcopy(rho0)
     for t_i = 2:length(rho_t)
-        t = T[t_i]
+        t = Tspan[t_i]
         rho_I = rho_t[t_i]
         U_op_S = evolutionOperator(Ham_sys, t)
         rho = U_op_S * rho_I * U_op_S'
