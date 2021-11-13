1deb3ce75a6e1de8e5b3d4757f43cbd19b1ce0f9
Daniel Herman <daniel.herman@homecredit.eu>
2021-11-14 00:59:18 +0100

    Added evolution of bath part in int

--- test/test_master_ansatz.jl
+++ test/test_master_ansatz.jl
@@ -30,13 +30,18 @@ import QuantumOpticsBase
     Ham = getAggHamiltonian(agg, aggInds, FCFact; groundState = true)
 
     Ham_bath = getAggHamiltonianBath(agg)
-    Ham_sys = getAggHamiltonianSystem(agg; groundState = false)
+    Ham_sys = getAggHamiltonianSystem(agg; groundState = true)
     b_sys = GenericBasis([size(Ham_sys, 1)])
     b_bath = GenericBasis([size(Ham_bath, 1)])
 
     Ham_int = getAggHamiltonianInteraction(agg, aggInds, FCFact; groundState = true)
     Ham_S = Ham - Ham_int
-
+    Ham_B = tensor(Ham_bath, OneDenseOperator(b_sys))
+    E0 = Ham_B.data[1, 1]
+    for I = 1:aggIndsLen
+        Ham_B.data[I, I] -= E0
+    end
+    Ham_B = DenseOperator(basis, basis, Ham_B.data)
     H_lambda, H_S = eigen(Ham_S.data)
     H_Sinv = inv(H_S)
     H_lambda = diagm(H_lambda)
@@ -54,20 +59,20 @@ import QuantumOpticsBase
     T = 300
     mu_array = [[2, 1]]
     W01 = thermal_state(T, mu_array, Ham, vibindices, aggInds; diagonalize=false, diagonal=true)
-    rho_traced1 = trace_bath(W01.data, agg, FCProd, aggInds, vibindices; groundState=false)
+    rho_traced1 = trace_bath(W01.data, agg, FCProd, aggInds, vibindices; groundState=true)
     mu_array = [[1, 2]]
     W02 = thermal_state(T, mu_array, Ham, vibindices, aggInds; diagonalize=false, diagonal=true)
-    rho_traced2 = trace_bath(W02.data, agg, FCProd, aggInds, vibindices; groundState=false)
+    rho_traced2 = trace_bath(W02.data, agg, FCProd, aggInds, vibindices; groundState=true)
 
     W0 = 1.0*W01 + 0.0*W02 
     W0_bath = get_rho_bath(W0, agg, FCProd, aggInds, vibindices; groundState=true)
-    rho0 = trace_bath(W0, agg, FCProd, aggInds, vibindices; groundState=false)
+    rho0 = trace_bath(W0, agg, FCProd, aggInds, vibindices; groundState=true)
 
     W0 = DenseOperator(W0.basis_l, W0.basis_r, complex(W0.data))
     W0_bath = DenseOperator(W0_bath.basis_l, W0_bath.basis_r, complex(W0_bath.data))
     rho0 = DenseOperator(rho0.basis_l, rho0.basis_r, complex(rho0.data))
 
-    p = (Ham_S, Ham_int, H_lambda, H_S, H_Sinv, W0, W0_bath, agg, FCProd, aggInds, vibindices, false, ComplexF64)
+    p = (Ham_S, Ham_int, H_lambda, H_S, H_Sinv, Ham_B, W0, W0_bath, agg, FCProd, aggInds, vibindices, true, ComplexF64)
     Tspan, rho_t = master_ansatz(
         rho0,
         tspan,
