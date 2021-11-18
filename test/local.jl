2f181b829641a0abbd74b262af6f5ef9d2de4a3e
Daniel Herman <daniel.herman@homecredit.eu>
2021-11-18 23:36:29 +0100

    Added revised version of interaction hamiltonian

--- test/local.jl
+++ test/local.jl
@@ -1,35 +1,28 @@
 using OpenQuantumSystems
 using LinearAlgebra
 using SparseArrays
-
+# using Plots
 
 mode1 = Mode(0.2, 1.0)
 Energy = [0.0, 200.0]
-mol1 = Molecule([mode1], 2, [2.0, 200.0])
-mol2 = Molecule([mode1], 2, [3.0, 300.0])
-agg = Aggregate([mol1, mol2])
+mol1 = Molecule([mode1], 3, [2.0, 200.0])
+mol2 = Molecule([mode1], 3, [3.0, 300.0])
+mol3 = Molecule([mode1], 3, [4.0, 400.0])
+agg = Aggregate([mol1, mol2, mol3])
 aggInds = getIndices(agg)
 FCFact = getFranckCondonFactors(agg, aggInds)
 
 agg.coupling[2, 3] = 50
 agg.coupling[3, 2] = 50
 
-Ham_S = getAggHamiltonianSystemSmall(agg; groundEnergy=true)
-print(size(Ham_S.data))
-print(Ham_S)
+println(aggInds)
 
-Ham_S = getAggHamiltonianSystemBig(agg, aggInds, FCFact; groundEnergy=true)
-print(size(Ham_S.data))
-print(Ham_S)
+Ham_I = getAggHamInteraction(agg)
+print(size(Ham_I.data))
+print(Ham_I)
+# heatmap(Ham_I.data)
+# show()
 
-Ham_B = getAggHamiltonianBathSmall(agg; groundEnergy=true)
-print(size(Ham_B.data))
-print(Ham_B)
-
-Ham_B = getAggHamiltonianBathBig(agg; groundEnergy=true)
-print(size(Ham_B.data))
-print(Ham_B)
-
-Ham_0 = getAggHamSystemBath(agg, aggInds, FCFact; groundEnergy=true)
-print(size(Ham_0.data))
-print(Ham_0)
+Ham = getAggHamiltonian(agg)
+print(size(Ham.data))
+print(Ham)
