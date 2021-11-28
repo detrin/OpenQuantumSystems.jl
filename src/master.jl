5233d873d843fc4dc5a1d29e5402f6be1f5e2cf5
Daniel Herman <daniel.herman@protonmail.com>
2021-11-28 22:58:58 +0100

    Fixing QME

--- src/master.jl
+++ src/master.jl
@@ -29,19 +29,19 @@ Integrate Quantum Master equation
 * `alg`: Algorithm with which DiffEqCallbacks will solve QME equation.
 """
 function master_int(
-    W0::T,
+    rho0::T,
     tspan::Array,
-    Ham_0::U,
-    Ham_I::V;
+    p;
     reltol::AbstractFloat = 1.0e-12,
     abstol::AbstractFloat = 1.0e-12,
-    int_reltol::AbstractFloat = 1.0e-8,
-    int_abstol::AbstractFloat = 0.0,
+    int_reltol::AbstractFloat = 1.0e-4,
+    int_abstol::AbstractFloat = 1.0e-4,
     alg::Any = DelayDiffEq.MethodOfSteps(DelayDiffEq.Vern6()),
     fout::Union{Function,Nothing} = nothing,
     kwargs...,
 ) where {B<:Basis,T<:Operator{B,B},U<:Operator{B,B},V<:Operator{B,B}}
     history_fun(p, t) = T(W0.basis_l, W0.basis_r, zeros(ComplexF64, size(W0.data)))
+    Ham_0, Ham_I, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, Ham_B, W0, W0_bath, agg, FCProd, indices, indicesMap, elementtype, aggCore, aggTools = p
     # (du,u,h,p,t)
     tmp = copy(W0.data)
     dmaster_(t, W::T, dW::T, history_fun, p) = dmaster_int(
@@ -51,16 +51,13 @@ function master_int(
         history_fun,
         tmp,
         p,
-        W0,
-        Ham_0,
-        Ham_I,
         int_reltol,
         int_abstol,
     )
     tspan_ = convert(Vector{float(eltype(tspan))}, tspan)
-    x0 = W0.data
-    state = T(W0.basis_l, W0.basis_r, W0.data)
-    dstate = T(W0.basis_l, W0.basis_r, W0.data)
+    x0 = rho0.data
+    state = T(rho0.basis_l, rho0.basis_r, rho0.data)
+    dstate = T(rho0.basis_l, rho0.basis_r, rho0.data)
     OpenQuantumSystems.integrate_delayed(
         tspan_,
         dmaster_,
@@ -69,6 +66,7 @@ function master_int(
         state,
         dstate,
         fout;
+        p = p,
         reltol = reltol,
         abstol = abstol,
         alg = alg,
@@ -83,48 +81,53 @@ function dmaster_int(
     history_fun,
     tmp::Array,
     p,
-    W0,
-    Ham_0::U,
-    Ham_I::V,
     int_reltol::AbstractFloat,
     int_abstol::AbstractFloat,
 ) where {B<:Basis,T<:Operator{B,B},U<:Operator{B,B},V<:Operator{B,B}}
+    Ham_0, Ham_I, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, Ham_B, W0, W0_bath, agg, FCProd, indices, indicesMap, elementtype, aggCore, aggTools = p
     Ham_II_t = getInteractionHamIPicture(Ham_0, Ham_I, t)
-    QuantumOpticsBase.mul!(dW, Ham_II_t, W0, -eltype(W)(im), zero(eltype(W)))
-    QuantumOpticsBase.mul!(dW, W0, Ham_II_t, eltype(W)(im), one(eltype(W)))
-
+    elementtype = eltype(W.data)
     kernel_integrated, err = QuadGK.quadgk(
-        s -> kernel_int(t, s, tmp, history_fun, p, Ham_II_t.data, Ham_0, Ham_I),
+        s -> kernel_int(t, s, tmp, history_fun, p, Ham_II_t.data),
         0,
         t,
         rtol = int_reltol,
         atol = int_abstol,
     )
-    LinearAlgebra.mul!(
-        dW.data,
-        -one(eltype(W)),
-        kernel_integrated,
-        one(eltype(W)),
-        one(eltype(W)),
-    )
+    # LinearAlgebra.mul!(tmp1, Ham_II_t.data, W0.data, -elementtype(im), zero(elementtype))
+    # LinearAlgebra.mul!(tmp1, W0.data, Ham_II_t.data, elementtype(im), one(elementtype))
+    K = Ham_II_t.data * W0.data - W0.data * Ham_II_t.data
+    K_traced = trace_bath(K, aggCore, aggTools)
+    kernel_integrated_traced = trace_bath(kernel_integrated, aggCore, aggTools)
+    dW.data[:, :] = -elementtype(im) * K_traced[:, :] - kernel_integrated_traced[:, :] 
+    # println(D(kernel_integrated , zero(kernel_integrated)))
     return dW
 end
 
-function kernel_int(t, s, tmp, h, p, Ham_II_t, Ham_0, Ham_I)
-    Ham_II_s = getInteractionHamIPictureA(Ham_0, Ham_I, s)
-    W_s = h(p, s)
+function kernel_int(t, s, tmp, h, p, Ham_II_t)
+    Ham_0, Ham_I, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, Ham_B, W0, W0_bath, agg, FCProd, indices, indicesMap, elementtype, aggCore, aggTools = p
+    # Ham_0, Ham_I, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, W0, W0_bath, agg, FCProd, indices, indicesMap, elementtype = p
+    Ham_II_s = getInteractionHamIPicture(Ham_0, Ham_I, s)
+    Ham_II_t = getInteractionHamIPicture(Ham_0, Ham_I, t)
+    rho_s = h(p, s)
 
-    if (typeof(W_s) <: Operator)
-        W_s = W_s.data
+    if (typeof(rho_s) <: Operator)
+        rho_s = rho_s.data
     end
-    dW = deepcopy(W_s)
     # commutator(Ham.data, commutator(Ham.data, tmp))
-    QuantumOpticsBase.mul!(tmp, Ham_II_s, W_s, eltype(W_s)(1), zero(eltype(W_s)))
-    QuantumOpticsBase.mul!(tmp, W_s, Ham_II_s, -eltype(W_s)(1), one(eltype(W_s)))
+    # QuantumOpticsBase.mul!(tmp, Ham_II_s, W_s, elementtype(1), zero(elementtype))
+    # QuantumOpticsBase.mul!(tmp, W_s, Ham_II_s, -elementtype(1), one(elementtype))
 
-    QuantumOpticsBase.mul!(dW, Ham_II_t, tmp, eltype(W_s)(1), zero(eltype(W_s)))
-    QuantumOpticsBase.mul!(dW, tmp, Ham_II_t, -eltype(W_s)(1), one(eltype(W_s)))
-    return dW
+    # QuantumOpticsBase.mul!(dW, Ham_II_t, tmp, elementtype(1), zero(elementtype))
+    # QuantumOpticsBase.mul!(dW, tmp, Ham_II_t, -elementtype(1), one(elementtype))
+    U_0_s = evolutionOperator(Ham_0+Ham_I, s)
+    W_bath_s = U_0_s * W0 * W0_bath'
+    U_0_op = evolutionOperator(Ham_0, s)
+    W0_int_s = U_0_op' * W_bath_s * U_0_op
+    # tmp2 .= ad(rho_s, W0_int_s.data, aggCore, aggTools)
+    C1 = Ham_II_s.data * W0_int_s.data - W0_int_s.data * Ham_II_s.data
+    C2 = Ham_II_t.data * C1 - C1 * Ham_II_t.data
+    return C2
 end
 
 """
--- src/master_ansatz.jl
+++ src/master_ansatz.jl
@@ -14,7 +14,7 @@ function master_ansatz(
 ) where {B<:Basis,T<:Operator{B,B}}
     history_fun(p, t) = T(rho0.basis_l, rho0.basis_r, zeros(ComplexF64, size(rho0.data)))
     # (du,u,h,p,t)
-    Ham_S, Ham_int, H_lambda, H_S, H_Sinv, Ham_B, W0, W0_bath, agg, FCProd, aggIndices, vibindices, elementtype, aggCore, aggTools = p
+    Ham_0, Ham_I, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, Ham_B, W0, W0_bath, agg, FCProd, aggIndices, vibindices, elementtype, aggCore, aggTools = p
     tmp1 = copy(W0.data)
     tmp2 = copy(W0.data)
     tmp3 = copy(W0.data)
@@ -52,19 +52,20 @@ function dmaster_ansatz(
     p,
     h
 ) where {B<:Basis,T<:Operator{B,B}}
-    Ham_S, Ham_int, H_lambda, H_S, H_Sinv, Ham_B, W0, W0_bath, agg, FCProd, aggIndices, vibindices, elementtype, aggCore, aggTools = p
+    Ham_0, Ham_I, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, Ham_B, W0, W0_bath, agg, FCProd, aggIndices, vibindices, elementtype, aggCore, aggTools = p
     # Ham_II_t = getInteractionHamIPicture(Ham_S, Ham_int, t)
-    Ham_II_t = getInteractionHamIPictureA(Ham_int.data, H_lambda, H_S, H_Sinv, t)
+    Ham_II_t = getInteractionHamIPictureA(Ham_I.data, Ham_0_lambda, Ham_0_S, Ham_0_Sinv, t)
     kernel_integrated, err = QuadGK.quadgk(
         s -> MemoryKernel(t, s, tmp1, tmp2, tmp3, h, p, Ham_II_t),
         0,
         t,
-        rtol = 1e-3,
-        atol = 1e-3
+        rtol = 1e-5,
+        atol = 1e-5
     )
     
-    LinearAlgebra.mul!(tmp1, Ham_II_t, W0_bath.data, -elementtype(im), zero(elementtype))
-    LinearAlgebra.mul!(tmp1, W0_bath.data, Ham_II_t, elementtype(im), one(elementtype))
+    tmp2 .= ad(rho.data, W0_bath.data, aggCore, aggTools)
+    LinearAlgebra.mul!(tmp1, Ham_II_t, tmp2, -elementtype(im), zero(elementtype))
+    LinearAlgebra.mul!(tmp1, tmp2, Ham_II_t, elementtype(im), one(elementtype))
     K_traced = trace_bath(tmp1, aggCore, aggTools)
     # rho0 = trace_bath(W0, agg, FCProd, aggIndices, vibindices)
     # println(t)
@@ -72,11 +73,12 @@ function dmaster_ansatz(
     # println(K_traced .* rho0.data)
 
     # println(data)
-    LinearAlgebra.mul!(drho.data, one(elementtype), K_traced, -elementtype(im), zero(elementtype))
-    data = kernel_integrated .* rho.data
-    LinearAlgebra.mul!(drho.data, one(elementtype), data, -elementtype(1), one(elementtype))
+    # LinearAlgebra.mul!(drho.data, one(elementtype), K_traced, -elementtype(im), zero(elementtype))
+    # data = kernel_integrated .* rho.data
+    # LinearAlgebra.mul!(drho.data, one(elementtype), kernel_integrated, -elementtype(1), one(elementtype))
     # println(-1im * (K_traced .* rho.data))
     # println(kernel_integrated .* rho.data)
+    drho.data[:, :] = K_traced[:, :] - kernel_integrated[:, :]
     
     return drho
 end
