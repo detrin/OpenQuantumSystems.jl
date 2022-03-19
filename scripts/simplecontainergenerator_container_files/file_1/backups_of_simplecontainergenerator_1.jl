af9ab5e962b57d4e51fec3a5a57ae6f02d8c19b4
Daniel Herman <daniel.herman@protonmail.com>
2022-03-19 14:56:25 +0100

    Building dockerfile turns out to be too slow

--- scripts/simplecontainergenerator_container_files/file_1/backups_of_simplecontainergenerator_1.jl
+++
@@ -1,5 +0,0 @@
-empty!(Base.DEPOT_PATH)
-pushfirst!(Base.DEPOT_PATH, "/opt/simplecontainergenerator_containers/depot_backup_simplecontainergenerator")
-import Pkg
-import Pkg; Pkg.add(Pkg.PackageSpec(name = "SimpleContainerGenerator"));
-
