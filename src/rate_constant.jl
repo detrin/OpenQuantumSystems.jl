55371fdcb566ea8950d091c5b8abb7771823d44b
Daniel Herman <daniel.herman@protonmail.com>
2022-05-05 23:23:54 +0200

    Moving RDM part to MK in QME.iter

--- src/rate_constant.jl
+++ src/rate_constant.jl
@@ -19,6 +19,7 @@ function M_aabb_W_bath_intp(t, s, p, tmp1, tmp2, Ham_II_t)
     Ham_0 = aggOperators.Ham_0
     Ham_I = aggOperators.Ham_I
     Ham_II_s = getInteractionHamIPicture(Ham_0, Ham_I, s)
+    rho_t = interpolate_with_tspan(rho_0_int_t_itp, tspan, t)
     W_bath_s = interpolate_with_tspan(W_0_bath_t_itp, tspan, s)
     
     elLen = aggCore.molCount+1
@@ -32,7 +33,7 @@ function M_aabb_W_bath_intp(t, s, p, tmp1, tmp2, Ham_II_t)
         tmp1[:, :] = Ham_II_t.data * tmp2 - tmp2 * Ham_II_t.data
         M_tr = trace_bath(tmp1, aggCore, aggTools; vib_basis=aggOperators.vib_basis)
         for a=1:elLen
-            M_aabb_[a, b] = M_tr[a, a]
+            M_aabb_[a, b] = M_tr[a, a] / rho_t[a, a]
         end
     end
     return M_aabb_
@@ -67,6 +68,7 @@ function M_abcd_W_bath_intp(t, s, p, tmp1, tmp2, Ham_II_t)
     Ham_0 = aggOperators.Ham_0
     Ham_I = aggOperators.Ham_I
     Ham_II_s = getInteractionHamIPicture(Ham_0, Ham_I, s)
+    rho_t = interpolate_with_tspan(rho_0_int_t_itp, tspan, t)
     W_bath_s = interpolate_with_tspan(W_0_bath_t_itp, tspan, s)
     
     elLen = aggCore.molCount+1
@@ -80,7 +82,7 @@ function M_abcd_W_bath_intp(t, s, p, tmp1, tmp2, Ham_II_t)
         tmp1[:, :] = Ham_II_t.data * tmp2 - tmp2 * Ham_II_t.data
         M_tr = trace_bath(tmp1, aggCore, aggTools; vib_basis=aggOperators.vib_basis)
         for a=1:elLen, b=1:elLen
-            M_abcd_[a, b, c, d] = M_tr[a, b]
+            M_abcd_[a, b, c, d] = M_tr[a, b] / rho_t[a, b]
         end
     end
     return M_abcd_
